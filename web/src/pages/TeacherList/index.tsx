ae3038186b987a912cd5c21b9adb9fbf20793278
Luis Humberto Rodrigues Gonçalves <luis.humberto@predict.info>
2020-08-08 18:17:01 -0300

    create: componente cabeçalho e formulario de professores

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import './styles.css'
+import PageHeader from '../../components/PageHeader'
 
-function TeacherList(){
-  return <h1>Teacher List</h1>
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Esses são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da Semana</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="hour">Hora</label>
+            <input type="text" id="hour" />
+          </div>
+        </form>
+      </PageHeader>
+    </div>
+  )
 }
 
 export default TeacherList
